eb4543018d32dc4235e84a93e13c1b3eb30d5e2d
sushmithdrokhade@gmail.com <sushmithdrokhade@gmail.com>
2026-06-23 17:29:04 +0530

    develop3

--- src/test/day2.java
+++ src/test/day2.java
@@ -36,6 +36,8 @@ public class day2 {
 		System.out.println("gitstuffdevelop 3");
 		System.out.println("master1");
 		System.out.println("master2");
+		System.out.println("develop312");
+		System.out.println("develop31234");
 
 		
 	}
